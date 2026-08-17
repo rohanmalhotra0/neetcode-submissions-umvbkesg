3e0c352c827ecef6f44add6a9a71f7f2408b51f5
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-08-17 13:35:36 -0400

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.replace(" ", "")
+        s = s.replace("?", "")
+        s = s.replace("!", "")
+        s = s.replace(".", "")
+        l = 0
+        r = len(s) - 1 
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1 
+        return True
