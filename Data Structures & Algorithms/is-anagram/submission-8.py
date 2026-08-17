b5d9ad7f62f792c8c078c75b2af96ca38c8040d8
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-08-17 13:28:36 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+      l = 0
+      r = len(s) - 1 
+      while l < r:
+        if s[l] != s[r]:
+            return False
+        l += 1
+        r -= 1 
+      return True
