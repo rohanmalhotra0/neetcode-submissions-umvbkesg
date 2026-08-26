d6d1d683d1d8ad9289d66d9e00a36b5ad366e487
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-08-26 12:12:53 -0400

    Add: islands-and-treasure - submission-14

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-14.py
@@ -0,0 +1,33 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols  = len(grid), len(grid[0])
+        visited = set()
+        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+        inf = 2147483647
+        
+        # More Efficent Use Array for Subproblems already computed
+        # take the min of every BFS
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    q.append((i,j))
+                    visited.add((i,j))
+   
+            while q:
+                x, y = q.popleft()
+                for dx, dy in directions:
+                    nx, ny = x + dx , y + dy
+                    
+                    if (
+                    0 <= nx < rows and 
+                    0 <= ny < cols and 
+                    (nx, ny) not in visited):
+                        
+                        if grid[nx][ny] == inf:
+                            q.append((nx,ny))
+                            visited.add((nx,ny))
+                            grid[nx][ny] = grid[x][y] + 1
+                            
+   
+            
+
