adf8c4c30082398a767d4e3fd3741a0e76a312e3
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-08-30 10:28:10 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t) or not t or not s:
+            return ""
+
+        countT = [0] * 100
+
+        minStr = s + 'a'
+        
+        for ch in t:
+            countT[ord(ch) - 65] += 1
+        
+        l = 0
+        countTDupe = countT.copy()
+        
+        for r in range(len(s)):
+            countT = countTDupe.copy()
+
+            if countT[ord(s[r]) - 65] >= 1 and countT:
+                l = r
+                #countT[ord(s[r]) - 65] -= 1
+                
+                while r < len(s):
+                    
+                    if countT[ord(s[r])- 65] >= 1 and countT:
+                        countT[ord(s[r]) - 65] -= 1
+                        
+                        if  max(countT) == 0:
+                            if len(minStr) > len(s[l:r+1]):
+                                minStr = s[l:r+1]
+                                break    
+                    r+=1
+        
+        if minStr != s + 'a':
+            return minStr
+        return ""
