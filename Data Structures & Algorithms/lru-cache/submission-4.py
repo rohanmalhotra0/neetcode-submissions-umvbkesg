701f6c359c3f31122e90ade739ea3ed29120d631
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-09-05 08:02:20 -0400

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,29 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.have = 0
+        self.capacity = capacity
+        self.stack = deque()
+        self.mapping = {}
+
+
+    def get(self, key: int) -> int:
+        if key in self.mapping:
+            return self.mapping[key]
+        else:
+            return -1
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.mapping:
+            self.mapping[key] = value
+        else:
+            if self.capacity > self.have:
+                self.mapping[key] = value
+                self.have += 1
+                self.stack.append(key)
+            else:
+                d = self.stack.popleft()
+                del self.mapping[d]
+                self.mapping[key] = value
+                self.stack.append(key)
