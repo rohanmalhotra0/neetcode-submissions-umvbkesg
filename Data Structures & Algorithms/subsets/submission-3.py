b41f9d61715b6409e0545aadd2a0b2023cc890b2
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-09-26 10:52:09 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def helper(self, i , subsets, currSet):
+        if i >= len(nums):
+            subsets.append(currSet.copy)
+            return
+        # Keep
+        helper(i+1, subsets, currSet)
+        # Skip 
+        while i < len(nums) and nums[i] == nums[i+1]:
+            i+=1
+        helper(i+1, subsets, currSet)
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        subsets, currSet = [], []
+        self.helper(0,subsets,currSet)
+        return subsets
+    
