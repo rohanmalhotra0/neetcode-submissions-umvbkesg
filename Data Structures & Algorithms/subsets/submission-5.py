a002d25eafbe9a71ea53b66d6f063fc6571d30bb
rohanmalhotra0 <104740676+rohanmalhotra0@users.noreply.github.com>
2026-09-26 10:53:10 -0400

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        subsets, currSet = [], []
+        self.helper(0,subsets,currSet,nums)
+        return subsets
+    def helper(self, i , subsets, currSet,nums):
+        if i >= len(nums):
+            subsets.append(currSet.copy)
+            return
+        # Keep
+        helper(i+1, subsets, currSet)
+        # Skip 
+        while i < len(nums) and nums[i] == nums[i+1]:
+            i+=1
+        helper(i+1, subsets, currSet)
+    
